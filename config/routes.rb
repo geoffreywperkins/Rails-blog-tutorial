44111468692cbe805e35792236c4c6c3de309fdf
Geoffrey Charles Witherington-Perkins <witherin@msu.edu>
2019-04-12 16:19:20 -0400

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
 
   root 'welcome#index'
 end
